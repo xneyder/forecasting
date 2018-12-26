ef62e753d02bcd8a08fc1715a45668f8bb9a4816
Helix Application Mediation User1 <med01@ilscha03-hlxmed-01.uscc.com>
2018-12-26 22:32:32 +0000

    initial commit

---
+++ forecasting.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+import cx_Oracle
+import base64
+import sys
+import os
+import pandas as pd
+import glob
+from datetime import datetime,timedelta
+from LoggerInit import LoggerInit
+from threading import Thread
+
+
+
+class ManagedDbConnection:
+        def __init__(self, DB_USER,DB_PASSWORD,ORACLE_SID,DB_HOST):
+                self.DB_USER = DB_USER
+                self.DB_PASSWORD = DB_PASSWORD
+                self.ORACLE_SID = ORACLE_SID
+                self.DB_HOST = DB_HOST
+
+        def __enter__(self):
+                try:
+                        self.db = cx_Oracle.connect('{DB_USER}/{DB_PASSWORD}@{DB_HOST}/{ORACLE_SID}'.format(DB_USER=self.DB_USER,DB_PASSWORD=self.DB_PASSWORD,DB_HOST=self.DB_HOST,ORACLE_SID=self.ORACLE_SID), threaded=True)
+                except cx_Oracle.DatabaseError as e:
+                        app_logger.error(e)
+                        quit()
+                self.cursor = self.db.cursor()
+                sqlplus_script="alter session set nls_date_format = 'YYYY-MM-DD HH24:MI'"
+                try:
+                        self.cursor.execute(sqlplus_script)
+                except cx_Oracle.DatabaseError as e:
+                        app_logger.error(e)
+                        app_logger.error(sqlplus_script[0:900])
+                        quit()
+                return self.db
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+                if self.cursor:
+                        self.cursor.close()
+                if self.db:
+                        self.db.close()
+
+def th_execute_sql_file(sql_file):
+	app_logger=logger.get_logger('th_execute_sql_file {sql_file}'.format(sql_file=os.path.basename(sql_file)))
+	app_logger.info('Processing')
+	with open(sql_file,'r') as file:
+		filedata=file.read()
+		for sqlplus_script in [sql for sql in filedata.split(';') if len(sql)>2]:
+			with ManagedDbConnection(DB_USER,DB_PASSWORD,ORACLE_SID,DB_HOST) as db:
+				cursor=db.cursor()
+				type=''
+				if 'insert' in sqlplus_script.lower():
+					type="inserted"
+				elif 'delete' in sqlplus_script.lower():
+					type="deleted"
+				#Loop from start date to end date day by day for performance
+				if '<start_date>' in sqlplus_script.lower():
+					today_date=datetime.today()	
+					end_date=today_date.replace(day=1,hour=0,minute=0,second=0,microsecond=0)-timedelta(days=1)
+					start_date=end_date.replace(day=1)
+					for single_date in pd.date_range(start_date,end_date):
+						end_date=single_date.replace(hour=23,minute=59)
+						t_sqlplus_script=sqlplus_script.replace('<start_date>',single_date.strftime("%Y-%m-%d %H:%M"))
+						t_sqlplus_script=t_sqlplus_script.replace('<end_date>',end_date.strftime("%Y-%m-%d %H:%M"))
+						try:
+							cursor.execute(t_sqlplus_script)
+							app_logger.info('{rowcount} Records {type} {single_date}'
+								.format(type=type,rowcount=cursor.rowcount,single_date=single_date.strftime("%Y-%m-%d %H:%M")))
+							db.commit()
+						except cx_Oracle.DatabaseError as e:
+							app_logger.error(str(e)+" --- "+t_sqlplus_script.replace('\n',' '))
+				else:
+					try:
+						cursor.execute(sqlplus_script)
+						app_logger.info('{rowcount} Records {type}'.format(type=type,rowcount=cursor.rowcount))
+						db.commit()
+					except cx_Oracle.DatabaseError as e:
+						app_logger.error(str(e)+" --- "+sqlplus_script.replace('\n',' '))
+					
+						
+			
+def main():
+	workers=[]
+	mask=os.path.join(SQL_DIR,'*sql')
+	for sql_file in glob.glob(mask):
+		worker = Thread(target=th_execute_sql_file,args=(sql_file,))
+		worker.setDaemon(True)
+		worker.start()
+		workers.append(worker)
+
+	for worker in workers:
+		worker.join()
+		
+if __name__ == '__main__':
+        DB_USER=os.environ['DB_USER']
+        DB_PASSWORD=base64.b64decode(os.environ['DB_PASSWORD'])
+        ORACLE_SID=os.environ['ORACLE_SID']
+        DB_HOST=os.environ['DB_HOST']
+	LOG_DIR=os.environ['LOG_DIR']
+	LOG_FILE=os.path.join(LOG_DIR,'cell_site_classification.log')
+	SQL_DIR=os.path.join(os.path.dirname(os.path.abspath(__file__)),'sql')
+	logger=LoggerInit(LOG_FILE,10)
+	app_logger=logger.get_logger('{script}'.format(script=sys.argv[0]))
+	app_logger.info('Start')
+	main()
---
+++ sql/prod/PALOALTO_OOBFW.Active_Sessions.sql
@@ -0,0 +1,36 @@
+--PALOALTO_OOBFW - Active Sessions
+delete from SMA_HLX.SMA_SUMMARY@SCHAHLXPRD 
+where SMA_NAME='PALOALTO_OOBFW' 
+and KPI_NAME='Active Sessions' 
+AND PERIOD_DATE=trunc(trunc(sysdate,'MM')-1,'MM');
+INSERT INTO SMA_HLX.SMA_SUMMARY@SCHAHLXPRD
+with pm_data as
+(
+	select /*+ materialize */ DATETIME, IP_NE_NAME, 
+	SUM(nvl(PAN_SESSION_ACTIVE,0)) KPI,
+	count(*) INSTANCE_COUNT
+	from PALOALTO_IP.PAN_IPNE_SESSIONSTAT_5M 
+	where IP_NE_NAME like '%-oobfw-%'
+	and DATETIME >= trunc(trunc(sysdate,'MM')-1,'MM') and DATETIME < trunc(sysdate,'MM')
+  	group by DATETIME, IP_NE_NAME
+) 
+select 
+trunc(datetime,'MM') PERIOD_DATE,
+'PALOALTO_OOBFW' SMA_NAME,
+'' REPORT_GROUP,
+'Core' REGION_GROUP,
+'Core' LOCATION_GROUP,
+'Active Sessions' KPI_NAME,
+'panSessionActive' INDICATOR_,
+'MM' TIME_AGG_TYPE,
+'PERC' MATH_AGG_TYPE,
+95 PERCENTILE_USED,
+PERCENTILE_CONT(0.95) within group (order by KPI) KPI_VALUE,
+'Counts' KPI_UNITS,
+300 RAW_POLLING_DURATION,
+count(IP_NE_NAME) PERIOD_COUNT,
+avg(INSTANCE_COUNT) AVG_INSTANCE_COUNT,
+sysdate REC_CREATE_DATE,
+sysdate LAST_UPDATE_DATE
+from pm_data
+group by trunc(datetime,'MM');
